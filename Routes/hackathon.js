c7430fc47cc36357841fb92c9e12c32992cfb089
Krushna-a <Krushnaboinwad70@gmail.com>
2025-06-27 16:01:30 +0530

    update cloudinary config

--- Routes/hackathon.js
+++ Routes/hackathon.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const hackRouter = express.Router();
-const { addHackathon, getHackathon,getIndividualHack,hackRegister,cancelRegister,addComment } = require("../Controllers/hackController");
+const {
+  addHackathon,
+  getHackathon,
+  getIndividualHack,
+  hackRegister,
+  cancelRegister,
+  addComment,
+} = require("../Controllers/hackController");
 const { storage } = require("../config/config");
 const multer = require("multer");
-const upload = multer({storage});
+const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
 
 hackRouter.post("/new", upload.single("bannerImage"), addHackathon);
-hackRouter.get("/getHackathon",getHackathon)
-hackRouter.get("/getIndividualHack/:hackId",getIndividualHack)
-hackRouter.post("/register",hackRegister)
-hackRouter.post("/cancel",cancelRegister)
-hackRouter.post("/addComment",addComment);
-
+hackRouter.get("/getHackathon", getHackathon);
+hackRouter.get("/getIndividualHack/:hackId", getIndividualHack);
+hackRouter.post("/register", hackRegister);
+hackRouter.post("/cancel", cancelRegister);
+hackRouter.post("/addComment", addComment);
 
 module.exports = hackRouter;
